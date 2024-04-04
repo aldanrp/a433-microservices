d46aa00f5bd5ad4ce4853b7f59c16953219f42e0
aldanrp <rallfirdan>
2024-04-04 12:52:54 +0700

    adding logs

--- command-compose-github-up.sh
+++ command-compose-github-up.sh
@@ -8,7 +8,7 @@ read GITHUB_USERNAME
 
 # docker login docker.pkg.github.com -u $GITHUB_USERNAME -p $GITHUB_TOKEN
 
-docker login docker.pkg.github.com -u $GITHUB_USERNAME  --password-stdin
+docker login docker.pkg.github.com -u $GITHUB_USERNAME -p --password-stdin
 
 # Run Docker Compose
 docker-compose -f docker-github-packages-compose.yml up -d
